1f093328309cc43eda4c5befefa718a6cf1a8642
MiracleWong <cfwr1991@126.com>
2023-11-09 15:45:22 +0800

    20231109 blog_service 添加处理程序：article.go、tag.go & 路由管理和启动

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"net/http"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/MiracleWong/go-blog-service/internal/routers"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"message": "Pong",
+	//	})
+	//})
+	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }

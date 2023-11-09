fc13f05d5cafd759957f63a4f525da95b95f31a2
MiracleWong <cfwr1991@126.com>
2023-11-09 18:03:10 +0800

    20231109 blog_service 配置管理：声明配置属性的结构体 + 读取配置的方法

---
+++ pkg/setting/section.go
@@ -0,0 +1,38 @@
+package setting
+
+import "time"
+
+// 声明配置属性的结构体
+
+// 服务器配置
+
+type ServerSettingS struct {
+	RunMode      string
+	HttpPort     string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+// 数据库配置
+
+type DatabaseSettingS struct {
+	DBType       string
+	UserName     string
+	Password     string
+	Host         string
+	DBName       string
+	Charset      string
+	ParseTime    bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+// 读取配置的方法
+
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}

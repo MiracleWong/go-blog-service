1707369a696c9ca2983d0e1228b91993e108ef68
MiracleWong <cfwr1991@126.com>
2023-11-09 18:03:32 +0800

    20231109 blog_service 配置管理：声明包全局变量

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/MiracleWong/go-blog-service/pkg/setting"
+
+// 声明全局变量
+var (
+	ServerSetting *setting.ServerSettingS
+	DataSetting   *setting.DatabaseSettingS
+)

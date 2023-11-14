81dbbcb68bb05c87ecfa267c1c7e792583ce5a61
MiracleWong <cfwr1991@126.com>
2023-11-14 18:25:34 +0800

    20231114 blog_service Log： WithFields、WithContext、WithCaller、WithCallersFrames

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,8 +2,10 @@ package logger
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
+	"runtime"
 )
 
 type Level int8
@@ -56,4 +58,51 @@ func NewLogger(w io.Writer, prefix string, flag int) *Logger {
 func (l *Logger) clone() *Logger {
 	nl := *l
 	return &nl
+}
+
+func (l *Logger) WithFields(f Fields) *Logger {
+	ll := l.clone()
+	if ll.fields == nil {
+		ll.fields = make(Fields)
+	}
+	for k, v := range f {
+		ll.fields[k] = v
+	}
+	return ll
+}
+
+func (l *Logger) WithContext(ctx context.Context) *Logger {
+	ll := l.clone()
+	ll.ctx = ctx
+	return ll
+}
+
+func (l *Logger) WithCaller(skip int) *Logger {
+	ll := l.clone()
+	pc, file, line, ok := runtime.Caller(skip)
+	if ok {
+		f := runtime.FuncForPC(pc)
+		ll.callers = []string{fmt.Sprintf("%s: %d %s", file, line, f.Name())}
+	}
+
+	return ll
+}
+
+func (l *Logger) WithCallersFrames() *Logger {
+	maxCallerDepth := 25
+	minCallerDepth := 1
+	callers := []string{}
+	pcs := make([]uintptr, maxCallerDepth)
+	depth := runtime.Callers(minCallerDepth, pcs)
+	frames := runtime.CallersFrames(pcs[:depth])
+	for frame, more := frames.Next(); more; frame, more = frames.Next() {
+		s := fmt.Sprintf("%s: %d %s", frame.File, frame.Line, frame.Function)
+		callers = append(callers, s)
+		if !more {
+			break
+		}
+	}
+	ll := l.clone()
+	ll.callers = callers
+	return ll
 }
